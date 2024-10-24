72afcb41cccd12c94c30ead9aeb5bf74d59c3ca1
nawazish-qaavi78 <123806130+nawazish-qaavi78@users.noreply.github.com>
2024-10-24 20:38:03 +0530

    experimenting with algo

--- experiment.c
+++ experiment.c
@@ -1,65 +1,67 @@
-
-/*
-* EcoMender Bot (EB): Task 2B Path Planner
-*
-* This program computes the valid path from the start point to the end point.
-* Make sure you don't change anything outside the "Add your code here" section.
-*/
-
-#include <stdlib.h>
+#include<stdio.h>
+#include <limits.h>
 #include <stdbool.h>
 #include <stdint.h>
-#include <limits.h>
+
 #define V 32
 
-#ifdef __linux__ // for host pc
-
-    #include <stdio.h>
-
-    void _put_byte(char c) { putchar(c); }
-
-    void _put_str(char *str) {
-        while (*str) {
-            _put_byte(*str++);
+int min_cost(int *cost, bool *processed) {
+    uint8_t min = UINT8_MAX;
+    int8_t index = -1;
+    for(uint8_t i = 0; i<V; i++){
+        if(!processed[i]) {
+            if(cost[i]<min){
+                index = i;
+                min = cost[i];
+            } 
         }
     }
+    return index;
+}
 
-    void print_output(uint8_t num) {
-        if (num == 0) {
-            putchar('0'); // if the number is 0, directly print '0'
-            _put_byte('\n');
-            return;
-        }
+void algo(uint8_t start_node, uint8_t end_node, uint32_t* graph){
+    // setting up the variables
+    uint8_t cost[V], parent[V];
+    bool processed[V];
 
-        if (num < 0) {
-            putchar('-'); // print the negative sign for negative numbers
-            num = -num;   // make the number positive for easier processing
-        }
+    for(uint8_t i = 0; i<V; i++){
+        parent[i] = -1;
+        processed[i] = false;
+        if(i != start_node) cost[i] = UINT8_MAX;
+        else cost[i] = 0;
+    }
 
-        // convert the integer to a string
-        char buffer[20]; // assuming a 32-bit integer, the maximum number of digits is 10 (plus sign and null terminator)
+    // starting the algo
+    for(uint8_t j = 0; j<V; j++){
         uint8_t index = 0;
-
-        while (num > 0) {
-            buffer[index++] = '0' + num % 10; // convert the last digit to its character representation
-            num /= 10;                        // move to the next digit
+        int8_t parent_index = 0;
+        parent_index = min_cost(cost, processed);
+        for(index = 0; index<V; index++){
+            if(graph[parent_index] & (1<<index)){
+                if(cost[index] > cost[parent_index] + 1) {
+                    cost[index] = cost[parent_index] + 1;
+                    parent[index] = parent_index;
+                }
+                if(index == end_node) break; // considering all of equal weight edges
+            }
         }
-        // print the characters in reverse order (from right to left)
-        while (index > 0) { putchar(buffer[--index]); }
-        _put_byte('\n');
+        processed[parent_index] = true;
+        if(index == end_node) break;
     }
+    
+    // decoding the output
+    uint8_t j = end_node;
+    while(j!=-1){
+        printf("%d\n", j);
+        j = parent[j];
+    }
+}
 
-    void _put_value(uint8_t val) { print_output(val); }
+int main(){
+    uint32_t graph[V];
 
-#else  // for the test device
 
-    void _put_value(uint8_t val) { }
-    void _put_str(char *str) { }
-
-#endif
-
-void set_graph(uint32_t *graph){
-    graph[0] =  (0b0000 << 28) |  // First 4 bits (MSB)
+     graph[0] = (0b0000 << 28) |  // First 4 bits (MSB)
                 (0b0000 << 24) |  // Next 4 bits
                 (0b0000 << 20) |  // Next 4 bits
                 (0b0000 << 16) |  // Next 4 bits
@@ -345,89 +347,10 @@ void set_graph(uint32_t *graph){
                 (0b0000 << 8)  |  // Next 4 bits
                 (0b0000 << 4)  |  // Next 4 bits
                 (0b0000);          // Last 4 bits (LSB)
-}
-
-// main function
-int main(int argc, char const *argv[]) {
-
-    #ifdef __linux__
-
-        const uint8_t START_POINT   = atoi(argv[1]);
-        const uint8_t END_POINT     = atoi(argv[2]);
-        uint8_t NODE_POINT          = 0;
-        uint8_t CPU_DONE            = 0;
-
-    #else
-        // Address value of variables for RISC-V Implementation
-        #define START_POINT         (* (volatile uint8_t * ) 0x02000000)
-        #define END_POINT           (* (volatile uint8_t * ) 0x02000004)
-        #define NODE_POINT          (* (volatile uint8_t * ) 0x02000008)
-        #define CPU_DONE            (* (volatile uint8_t * ) 0x0200000c)
-
-    #endif
-
-    // array to store the planned path
-    uint8_t path_planned[32];
-    // index to keep track of the path_planned array
-    uint8_t idx = 0;
-
-    /* Functions Usage
-
-    instead of using printf() function for debugging,
-    use the below function calls to print a number, string or a newline
-
-    for newline: _put_byte('\n');
-    for string:  _put_str("your string here");
-    for number:  _put_value(your_number_here);
-
-    Examples:
-            _put_value(START_POINT);
-            _put_value(END_POINT);
-            _put_str("Hello World!");
-            _put_byte('\n');
-    */
-
-    // ############# Add your code here #############
-    // prefer declaring variable like this
-    #ifdef __linux__
-        uint32_t graph[32];
-    #else
-        uint32_t *graph = (uint32_t *) 0x02000010;
-    #endif
-
-    set_graph(graph);
-    
-    
-
-    
 
 
-
-
-
-    
-
-
-
-    // ##############################################
-
-    // the node values are written into data memory sequentially.
-    for (int i = 0; i < idx; ++i) {
-        NODE_POINT = path_planned[i];
-    }
-    // Path Planning Computation Done Flag
-    CPU_DONE = 1;
-
-    #ifdef __linux__    // for host pc
-
-        _put_str("######### Planned Path #########\n");
-        for (int i = 0; i < idx; ++i) {
-            _put_value(path_planned[i]);
-        }
-        _put_str("################################\n");
-
-    #endif
+    uint8_t start_node = 0, end_node = 5;
+    algo(start_node, end_node, graph);
 
     return 0;
 }
-
